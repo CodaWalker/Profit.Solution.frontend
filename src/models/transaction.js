9e00986831add6f3f097d9709a58d2aba52a1d7c
CodaWalker <fierymail@list.ru>
2019-12-27 00:42:17 +0300

    Delete transaction.js

--- src/models/transaction.js
+++
@@ -1,8 +0,0 @@
-export class Transaction {
-    constructor(userId, course, dateOfIssue, id) {
-        this.userId = userId;
-        this.course = course;
-        this.dateOfIssue = dateOfIssue;
-        this.id = id;
-    }
-}
